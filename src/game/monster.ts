e07f3b46234b21cf7e54e99e1e7032c4bb2df9ea
AndyOsuna <57018426+AndyOsuna@users.noreply.github.com>
2023-11-15 17:23:27 -0300

    Fix bug at monster's attack

--- src/game/monster.ts
+++ src/game/monster.ts
@@ -2,25 +2,20 @@ import Character from "./character";
 import Weapon from "./weapon";
 
 export class Monster extends Character {
-isAttacking= false;
-  
+  isAttacking = false;
+
   name = "Monster";
   constructor(HP: number, weapon?: Weapon) {
     super(HP, 1, 1, weapon);
   }
 
   attack(characterAttacked: Character) {
- 
-   {
-    this.isAttacking = true; 
+    this.isAttacking = true;
     super.attack(characterAttacked);
-    
-   
+
     setTimeout(() => {
       this.isAttacking = false;
-    }, 1000); 
-}
-    super.attack(characterAttacked);
+    }, 1000);
   }
   receiveDamage(damage: number): void | Weapon {
     super.receiveDamage(damage);
